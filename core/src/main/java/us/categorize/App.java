e0324983d68b1939d701ccea65ac7cda6e0ae8c5
keefe roedersheimer <keefe@categorize.us>
2016-06-04 14:03:37 -0700

    verifying that embedded servlet is working properly

--- core/src/main/java/us/categorize/App.java
+++ core/src/main/java/us/categorize/App.java
@@ -1,13 +1,22 @@
 package us.categorize;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+        Server server = new Server(8080); 
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        handler.addServletWithMapping(MessageServlet.class, "/*");
+        server.start();
+        server.join();
     }
 }
