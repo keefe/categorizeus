3f7a4daed8a7489eada411c98b9406c60be0e299
keefe roedersheimer <keefe@categorize.us>
2016-06-04 14:03:37 -0700

    verifying that embedded servlet is working properly

---
+++ core/src/main/java/us/categorize/MessageServlet.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package us.categorize;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author keefe
+ *
+ */
+public class MessageServlet extends HttpServlet {
+	@Override
+    protected void doGet( HttpServletRequest request,
+                          HttpServletResponse response ) throws ServletException,
+                                                        IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Checking that wiring is up?</h1>");
+    }
+}
