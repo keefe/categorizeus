8d23ff5baf6760435a1a1a2663cfba7e290e6e20
keefe roedersheimer <keefe@categorize.us>
2016-06-04 17:15:01 -0700

    adding postgres and verifying that setup is good

--- core/src/main/java/us/categorize/App.java
+++ core/src/main/java/us/categorize/App.java
@@ -1,5 +1,10 @@
 package us.categorize;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -7,7 +12,6 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 /**
  * Hello world!
  *
@@ -19,6 +23,24 @@ public class App
     	ObjectMapper mapper = new ObjectMapper();
     	JsonNode actualObj = mapper.readTree("{\"k1\":\"v1\"}");
     	System.out.println("Verifying Jackson Install " + actualObj.get("k1").asText());
+    	
+    	String dbName = System.getenv("CATEGORIZEUS_DB");
+    	String dbUser = System.getenv("CATEGORIZEUS_DB_USER");
+    	String dbPass = System.getenv("CATEGORIZEUS_DB_PASS");
+    	
+    	Class.forName("org.postgresql.Driver");
+    	Connection conn = DriverManager.getConnection("jdbc:postgresql:"+dbName, dbUser, dbPass);
+    	
+    	Statement st = conn.createStatement();
+    	ResultSet rs = st.executeQuery("SELECT * FROM testing");
+    	while (rs.next())
+    	{
+    	   System.out.print("Column 1 returned ");
+    	   System.out.println(rs.getString(1));
+    	} rs.close();
+    	st.close();
+    	
+    	conn.close();
     	int port = Integer.parseInt(System.getenv("CATEGORIZEUS_PORT"));
     	System.out.println("Starting Server on Port " + port);
         Server server = new Server(port); 
