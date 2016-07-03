7d56c6a46caf9d688de5f8a23ef12e74d37d9b78
keefe roedersheimer <keefe@categorize.us>
2016-07-03 11:07:37 -0700

    basic ability to load and display a doc

--- static/scripts/categorizeus.js
+++ static/scripts/categorizeus.js
@@ -1,3 +1,26 @@
+var tmplBasicDocument;
+
+var loadDocument = function(id, cb){
+	$.ajax({
+		url:'/msg/'+id,
+		accepts:'application/json'
+	}).done(function(message, statusCode){
+		console.log("In Response " + statusCode);
+		if(cb){
+			cb(message);
+		}
+	});
+
+};
+
 $(document).ready(function(){
 	console.log("We're up");
+	tmplBasicDocument = Handlebars.compile($("#tmplBasicDocument").html());
+
+	$("#btnLoadDoc").click(function(){
+		var id = $("#txtDocId").val();
+		loadDocument(id, function(doc){
+			$("#content").html(tmplBasicDocument(doc));
+		});
+	});
 });
