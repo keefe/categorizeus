693c9dcd51ce56a0000ae59bf4bcc59e27c025ef
keefe roedersheimer <keefe@categorize.us>
2016-07-10 15:42:27 -0700

    basic tag messages, lookup tagged messages

--- core/src/main/java/us/categorize/TagServlet.java
+++ core/src/main/java/us/categorize/TagServlet.java
@@ -2,6 +2,7 @@ package us.categorize;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -41,7 +42,47 @@ public class TagServlet extends HttpServlet {
             HttpServletResponse response ) throws ServletException,
     IOException
     {
+		JsonNode bodyObj = ServletUtil.readyBody(request);
+		JsonNode tagNode = bodyObj.get("tags");
+		String tags[] = null;		
+		if(tagNode.isArray()){
+			tags = new String[tagNode.size()];
+			int i =0;
+			for(JsonNode aTag : tagNode){
+				tags[i++] = aTag.asText();
+			}
+		}
 		
+		JsonNode messagesNode = bodyObj.get("messages");
+		long messages[] = null;
+		if(messagesNode.isArray()){
+			messages = new long[messagesNode.size()];
+			int i = 0;
+			for(JsonNode aMessage : messagesNode){
+				messages[i++] = Long.parseLong(aMessage.asText());
+			}
+		}
+		if(messages!=null && tags!=null){
+			try {
+				Tag tagObjs[] = tagRepository.tagsFor(tags);
+				messageRepository.tag(messages, tagObjs);
+		        response.setStatus(HttpServletResponse.SC_OK);
+		        response.getWriter().println("OK");
+		        response.getWriter().close();
+		        return;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}			
+		}
+		
+
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        response.getWriter().println("uhm, we done fucked up");
+        response.getWriter().close();		
+		
+		//some kind of error handling or a 500 or something here
+				
     }
 	
 	@Override
@@ -51,7 +92,6 @@ public class TagServlet extends HttpServlet {
     {
 		JsonNode bodyObj = ServletUtil.readyBody(request);
 		JsonNode tagNode = bodyObj.get("tags");
-		long messageId = bodyObj.get("messageId").asLong();
 		if(tagNode.isArray()){
 			String tags[] = new String[tagNode.size()];
 			int i =0;
@@ -60,13 +100,22 @@ public class TagServlet extends HttpServlet {
 			}
 			try {
 				Tag tagObjs[] = tagRepository.tagsFor(tags);
-				Message message = messageRepository.getMessage(messageId);
-				messageRepository.tag(message, tagObjs);
+				List<Message> messages = messageRepository.findMessages(tagObjs);
+				ObjectMapper mapper = new ObjectMapper();
+				String messageString = mapper.writeValueAsString(messages.toArray(new Message[]{}));
+		        response.setContentType("application/json");
+		        response.setStatus(HttpServletResponse.SC_OK);
+		        response.getWriter().println(messageString);
+		        response.getWriter().close();
+		        return;
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        response.getWriter().println("uhm, we done fucked up");
+        response.getWriter().close();
 		//some kind of error handling or a 500 or something here
 		
     }
