871b7164a69ede3d54ca7dccd457f9df80554445
keefe roedersheimer <keefe@categorize.us>
2016-09-18 14:35:24 -0700

    preparing for thread implementation

--- core/src/main/java/us/categorize/repository/MessageRepository.java
+++ core/src/main/java/us/categorize/repository/MessageRepository.java
@@ -5,18 +5,16 @@ import java.util.List;
 import us.categorize.model.Message;
 import us.categorize.model.MessageThread;
 import us.categorize.model.Tag;
+import us.categorize.model.ThreadCriteria;
 
 //#TODO pagination, using continuation token style
 //#TODO add support for saving, deleting messages, even if not exposed at the web layer
 public interface MessageRepository {
 	Message getMessage(long id) throws Exception;
 	boolean addMessage(Message message);//are messages immutable?
-	boolean postMessage(String body);//#TODO figure this out, for the post stuff
 	boolean updateMessage(Message message);
-	MessageThread getThread(String id);
 	List<Message> findMessages(Tag[] tags);
-	List<Message> findThreads(List<Tag> tags);
+	MessageThread loadThread(ThreadCriteria criteria);
 	boolean tag(Message message, Tag[] tags);
 	boolean tag(long messageIds[], Tag[] tags);
-	
 }
