21060bff4c7263582c0645d17f4b3a8a897f11d6
keefe roedersheimer <kroeders@gmail.com>
2017-09-17 17:43:02 -0700

    fix twitter login wiring

--- core/src/main/java/us/categorize/server/http/TwitterSigninCallbackServlet.java
+++ core/src/main/java/us/categorize/server/http/TwitterSigninCallbackServlet.java
@@ -6,7 +6,7 @@ import twitter4j.*;
 import twitter4j.auth.*;
 import java.io.*;
 
-public class TwitterSigninCallbackServlet{
+public class TwitterSigninCallbackServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Twitter Signin Callback Servlet Handling GET");
         Twitter twitter = (Twitter) request.getSession().getAttribute("twitter");
@@ -15,6 +15,7 @@ public class TwitterSigninCallbackServlet{
         try {
             twitter.getOAuthAccessToken(requestToken, verifier);
             request.getSession().removeAttribute("requestToken");
+            System.out.println("Logged in with Twitter As " + twitter.getScreenName() + " id " + twitter.getId());
         } catch (TwitterException e) {
             throw new ServletException(e);
         }
